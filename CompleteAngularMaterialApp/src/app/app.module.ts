6343031233d64b1fbdc70de80a9d616f23cc6c14
DotnetMob <dotnetmob@gmail.com>
2018-09-21 19:16:51 +0530

    Angular Material

---
+++ CompleteAngularMaterialApp/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from "./material/material.module";
+import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './shared/employee.service';
+import { environment } from '../environments/environment';
+import { DepartmentService } from './shared/department.service';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    MatConfirmDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FormsModule
+  ],
+  providers: [EmployeeService,DepartmentService],
+  bootstrap: [AppComponent],
+  entryComponents:[EmployeeComponent,MatConfirmDialogComponent]
+})
+export class AppModule { }
